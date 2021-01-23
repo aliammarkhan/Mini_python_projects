eaf9fa8aa6d6a12524b84bb32b34dcab9d75de05
Ali Ammar Khan <50051546+aliammarkhan@users.noreply.github.com>
2021-01-23 20:53:48 +0500

    Update create_user.py

--- mini python projects/habbit_tracking/create_user.py
+++ mini python projects/habbit_tracking/create_user.py
@@ -3,8 +3,8 @@ endpoint = 'https://pixe.la/v1/users'
 
 #docs https://docs.pixe.la/entry/post-user
 params = {
-    "token" :  "YOUR_USERNAME_GOES_HERE",
-    "username": "biohazard",
+    "token" :  "YOUR_TOKEN_GOES_HERE",
+    "username": "YOUR_USERNAME_GOES_HERE",
     "agreeTermsOfService" : "yes",
     "notMinor" : "yes"
 }
